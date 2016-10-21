5baabc2a512935d4fd802c5cb5eea16e1508bf84
danksalot <danksalot@users.noreply.github.com>
2016-10-20 21:51:17 -0500

    Create stringManipulator.cpp

---
+++ src/stringManipulator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include "functions.h"
+
+using namespace std;
+
+string StringManipulator::ConvertToUpper(string input)
+{
+    transform(input.begin(), input.end(), input.begin(), ::toupper);
+    return input;
+}
+
+string StringManipulator::ConvertToLower(string input)
+{
+    transform(input.begin(), input.end(), input.begin(), ::tolower);
+    return input;
+}
