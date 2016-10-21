9490cafba674cf8743f89020803cc0b9d784e84a
danksalot <danksalot@users.noreply.github.com>
2016-10-20 21:50:33 -0500

    Create stringManipulator.h

---
+++ src/stringManipulator.h
@@ -0,0 +1,11 @@
+#ifndef STRING_MANIPULATOR_H
+#define STRING_MANIPULATOR_H
+
+class StringManipulator
+{
+    public:
+    std::string ConvertToUpper(std::string input);
+    std::string ConvertToLower(std::string input);
+};
+
+#endif
