990d836454ba3acf6d16b3f6b1fbf5a860af04c3
danksalot <danksalot@users.noreply.github.com>
2016-11-01 10:10:39 -0500

    Update person.cpp

--- src/person.cpp
+++ src/person.cpp
@@ -8,10 +8,10 @@ bool Person::IsValid()
     return FirstName.size() < 10 && LastName.size() < 10 && DateOfBirth.size() == 10;
 }
 
-bool Sort(Person a, Person b)
-{
-    return a < b;
-}
+//bool Sort(Person a, Person b)
+//{
+//    return a < b;
+//}
 
 string Person::GetDetails()
 {
